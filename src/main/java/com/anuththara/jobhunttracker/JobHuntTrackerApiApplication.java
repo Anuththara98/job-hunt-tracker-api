02e7a56893f1b626b2016834db08789925b302a8
Anuththara98 <anukavindi@gmail.com>
2026-04-28 20:32:43 +1200

    Job Tracker API

---
+++ src/main/java/com/anuththara/jobhunttracker/JobHuntTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.anuththara.jobhunttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobHuntTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobHuntTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuththara/jobhunttracker/JobHuntTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuththara.jobhunttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobHuntTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
